453f4d7667d3cec64a528b2de6f02cd0d7e4075e
bamboosdu <bamboosdu@gmail.com>
2020-09-16 18:29:15 +0800

    first commit

---
+++ run_yolo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#python3 trt_yolo.py --usb 0 -m yolov3-416
+python3 trt_yolo.py --image /home/iking/zq/git-space/tensorrt_test/myData/frameimage1.jpg -m yolov3-416
---
+++ yolo/onnx2trt.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 onnx_to_tensorrt.py -m darknet/yolov3-416
